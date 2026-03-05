b2b53d700be8d49e565357213473581ad3d6ba43
badpanda83 <michael83smith@gmail.com>
2026-03-05 11:42:16 -0500

    chore: remove stale winsvc.go and winsvc_stub.go superseded by winsvc_windows.go and winsvc_other.go

--- winsvc_stub.go
+++
@@ -1,8 +0,0 @@
-//go:build !windows
-
-package main
-
-// runAsWindowsService is a no-op on non-Windows platforms.
-func runAsWindowsService(_ string) (bool, <-chan struct{}) {
-	return false, nil
-}
